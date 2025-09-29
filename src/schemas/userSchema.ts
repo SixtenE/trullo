be0a1c2596507e2c0207f92b3208557bf5a52202
SixtenE <sixten.ekblad@gmail.com>
2025-09-29 20:13:01 +0200

    password (minst 8 tecken, lagras inte i klartext, använd bcrypt ex.)

--- src/schemas/userSchema.ts
+++ src/schemas/userSchema.ts
@@ -5,7 +5,7 @@ export const UserSchema = z.object({
   _id: z.string().refine((val) => isValidObjectId(val)),
   name: z.string().min(2).max(100),
   email: z.email(),
-  password: z.string().min(6).max(100),
+  password: z.string().min(8).max(100),
 });
 
 export const CreateUserSchema = UserSchema.omit({
